9bb9f75966621f8d3d833ec71ccea34e80e3c748
Juanm4R <110467078+Juanm4R@users.noreply.github.com>
2022-10-13 09:05:17 -0300

    Initial commit

---
+++ Assets/PersistentObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentObjects : MonoBehaviour
+{
+    public static PersistentObjects Instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
